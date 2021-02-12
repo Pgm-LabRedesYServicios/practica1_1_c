4459fba4db21840d9fe0c46cdaf19d3ced2e1a20
zentauro <pedrogomezmartin1999@gmail.com>
2021-02-12 16:04:06 +0100

    ping_oc: Connect and handle errors

--- src/ping_oc.c
+++ src/ping_oc.c
@@ -1,3 +1,5 @@
+#include <err.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,8 +18,18 @@ int main(int argc, char *argv[]) {
   usage_client(argc, argv);
 
   serv_addr.sin_family = AF_INET;
-  parse_port(&serv_addr ,argv[2]);
+  parse_port(&serv_addr, argv[2]);
   parse_addr(&serv_addr, argv[1]);
+
+  errno = 0;
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock_fd == -1) {
+    err(errno, "Error: Failed to create socket\n");
+  }
+
+  if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    err(errno, "Error: Failed to connect\n");
+  }
 
   return 0;
 }
