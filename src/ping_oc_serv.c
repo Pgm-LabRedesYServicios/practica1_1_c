a3c532b8f45eb45265bb807d70a88d75d3115eb2
zentauro <pedrogomezmartin1999@gmail.com>
2021-02-12 18:04:51 +0100

    ping_oc_serv: Initial non working implementation

--- src/ping_oc_serv.c
+++ src/ping_oc_serv.c
@@ -1,2 +1,55 @@
+#include <err.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) { return 0; }
+#include "parser.h"
+#include "usage_server.h"
+
+int main(int argc, char *argv[]) {
+  int sock_fd, newsock_fd, port_n, cli_len, chars_read, conn;
+  char buffer[512] = {0};
+  struct sockaddr_in serv_addr = {0}, clie_addr = {0};
+
+  usage_server(argc, argv);
+
+  // TODO: Find a way to resolve an url to IPv4 addr
+
+  serv_addr.sin_family = AF_INET;
+  parse_port(&serv_addr, argv[1]);
+
+  errno = 0;
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock_fd == -1) {
+    err(errno, "Error: Failed to create socket");
+  }
+
+  if (bind(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    err(errno, "Error: Failed to connect");
+  }
+
+  listen(sock_fd, 0);
+
+  while(!(conn = accept(sock_fd, (struct sockaddr*)&clie_addr, NULL))) {
+    int ret = 1;
+    while (ret) {
+      // TODO: Implement timeout
+      ret = recv(sock_fd, buffer, 512 * sizeof(char), 0);
+      if(ret < 0) {
+        close(sock_fd);
+        err(errno, "Error: Failed to read from connection");
+      }
+
+      send(sock_fd, buffer, 8, 0);
+    }
+  }
+
+  close(sock_fd);
+
+  return 0;
+}
