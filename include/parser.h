6acaec43818f36ed09ec20b0cabcb35ca5dbe475
zentauro <pedrogomezmartin1999@gmail.com>
2021-02-12 15:11:39 +0100

    Address parser

--- include/parser.h
+++ include/parser.h
@@ -1,13 +1,28 @@
 #ifndef INCLUDE_PARSER_H
 #define INCLUDE_PARSER_H
 
+#include <netinet/in.h>
+
 /**
- * @brief Parses a string into an int if it fails, it exits with 1
+ * @brief Parses a string into an int in network endianess. If it
+ *        fails, it exits the program with 1
  *
  * @param port_string The string to be parsed into an integer
  *
  * @return The port number
  */
 int parse_port(char *port_string);
+
+/**
+ * @brief Parses a string into a sockaddr_in and if it fails, it exits
+ *        with 1
+ *
+ * @param addr      The string to be parsed into a sockaddr_in
+ * @out   serv_addr The struct to store the result of the parsed
+ *                  string
+ *
+ * @return The port number
+ */
+void parse_addr(struct sockaddr_in *serv_addr, char *addr);
 
 #endif // INCLUDE_PARSER_H
--- src/parser.c
+++ src/parser.c
@@ -1,20 +1,15 @@
-#include <parser.h>
-
+#include <arpa/inet.h>
 #include <err.h>
 #include <errno.h>
 #include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * @brief Parses a string into an int if it fails, it exits with 1
- *
- * @param port_string The string to be parsed into an integer
- *
- * @return The port number
- */
+#include "parser.h"
+
 int parse_port(char *port_string) {
   int res;
-  char* invalid_addr;
+  char *invalid_addr;
 
   errno = 0;
   res = strtol(port_string, &invalid_addr, 10);
@@ -25,4 +20,37 @@ int parse_port(char *port_string) {
   res = htons(res);
 
   return res;
+}
+
+void parse_addr(struct sockaddr_in *serv_addr, char *addr) {
+  int res;
+
+  // Clear errno since inet_pton might set it
+  errno = 0;
+  res = inet_pton(AF_INET, addr, &serv_addr->sin_addr);
+
+  switch (res) {
+  case 1: {
+    // Valid network address
+    return;
+  }
+
+  case 0: {
+    // The address provided is not valid IPv4
+    fprintf(stderr, "Error: The address: %s is invalid for IPv4\n", addr);
+    exit(1);
+
+    break;
+  }
+
+  case -1: {
+    // This should never happen since we are providing AF_INET
+    err(errno, "This should not happen");
+  }
+
+  default: {
+    fprintf(stderr, "Error: something went wrong when parsing %s to an IPv4 address\n", addr);
+    exit(1);
+  }
+  }
 }
