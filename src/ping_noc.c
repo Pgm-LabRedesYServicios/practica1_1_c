3b1ec55e897bba472c455a674df81077480795a1
zentauro <pedrogomezmartin1999@gmail.com>
2021-02-25 12:00:37 +0100

    ping_noc: Done

--- src/ping_noc.c
+++ src/ping_noc.c
@@ -1,1 +1,81 @@
-int main(int argc, char *argv[]) { return 0; }
+#include <err.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "parser.h"
+#include "usage_client.h"
+
+int main(int argc, char *argv[]) {
+  int sock_fd, newsock_fd, port_n, cli_len, chars_read;
+  char buffer[512] = {0};
+  buffer[0] = 'P';
+  buffer[1] = 'I';
+  buffer[2] = 'N';
+  buffer[3] = 'G';
+  buffer[4] = '\n';
+  struct sockaddr_in serv_addr = {0};
+
+  // Check for arguments and print usage if there is a wrong number of
+  // arguments
+  usage_client(argc, argv);
+
+  serv_addr.sin_family = AF_INET;
+  parse_port(&serv_addr, argv[2]);
+  parse_addr(&serv_addr, argv[1]);
+
+  errno = 0;
+  sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock_fd == -1) {
+    err(errno, "Error: Failed to create socket");
+  }
+
+  if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    err(errno, "Error: Failed to connect");
+  }
+
+  printf("PING %s 56 bytes of data\n", argv[1]);
+  for (uint8_t i = 1; i < 9; i++) {
+    struct timespec t0, t1;
+    double elapsed;
+    int bsent, brecv;
+
+    // Get t0 to calculate time delta
+    // NOTE: It is done with the monotonic clock to avoid t1 < t0
+    clock_gettime(CLOCK_MONOTONIC, &t0);
+
+    // Send to server and check for errors
+    bsent = send(sock_fd, buffer, 8, 0);
+    if(bsent == -1) {
+      close(sock_fd);
+      err(errno, "Errro while writing to socket");
+    }
+
+    // Received bytes and check for errors
+    brecv = recv(sock_fd, buffer, 512 * sizeof(char), 0);
+    if (brecv == -1) {
+      close(sock_fd);
+      err(errno, "Errro while reading from socket");
+    }
+
+    // Get t1 to calculate time delta and the print it
+    // NOTE: It is done with the monotonic clock to avoid t1 < t0
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    if (t1.tv_sec - t0.tv_sec) {
+      printf("%d bytes from %s: time=%lds\n", 8, argv[1], t1.tv_sec - t0.tv_sec );
+    } else {
+      elapsed = t1.tv_nsec - t0.tv_nsec;
+      printf("%d bytes from %s: time=%lfms\n", 8, argv[1], elapsed / 1000);
+    }
+  }
+
+  close(sock_fd);
+
+  return 0;
+}
--- src/ping_noc_serv.c
+++ src/ping_noc_serv.c
@@ -1,2 +1,84 @@
+#include <err.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) { return 0; }
+#include "parser.h"
+#include "usage_server.h"
+
+int main(int argc, char *argv[]) {
+  int sock_fd, newsock_fd, port_n, cli_len, chars_read;
+  char buffer[512] = {0};
+  struct sockaddr_in serv_addr = {0}, clie_addr = {0};
+  socklen_t size = sizeof(clie_addr);
+
+  // Set recv address struct to send back to the client
+  clie_addr.sin_family = AF_INET;
+  clie_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  clie_addr.sin_port = htons(7654);
+
+  // Check for arguments and print usage if there is a wrong number of
+  // arguments
+  usage_server(argc, argv);
+
+  // Try to create an IPv4 sockaddr structure from the command line
+  // arguments
+  serv_addr.sin_family = AF_INET;
+  parse_port(&serv_addr, argv[1]);
+
+  // Clear errno and check for socket creation errors
+  errno = 0;
+  sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock_fd == -1) {
+    err(errno, "Error: Failed to create socket");
+  }
+
+  // Try to bind to the specified address
+  if (bind(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    err(errno, "Error: Failed to connect");
+  }
+
+  printf("Listening on port %s\n", argv[1]);
+
+  // Loop until the client closes the connection
+  while (1) {
+    int brecv, bsent;
+
+    // Receive bytes
+    brecv = recvfrom(sock_fd, buffer, 512 * sizeof(char), 0,
+                     (struct sockaddr *)&clie_addr, &size);
+
+    // If brecv is -1 something went wrong, free resources and abort
+    if (brecv < 0) {
+      close(sock_fd);
+      err(errno, "Error: Failed to read from connection");
+    } else {
+      // If brecv is 0, that means that the connection is closed,
+      // break out of the loop to keep awaiting to accept new
+      // connections
+      if (brecv == 0) {
+        close(sock_fd);
+        printf("[i] The client has closed the connection\n");
+        break;
+      }
+    }
+
+    printf("[i] Received %d bytes\n", brecv);
+
+    // Try send the same that the client sent us
+    bsent = sendto(sock_fd, buffer, 8, 0, (struct sockaddr *)&clie_addr, size);
+    if (bsent == -1) {
+      close(sock_fd);
+      err(errno, "Error: Failed to send");
+    }
+  }
+
+  close(sock_fd);
+
+  return 0;
+}
